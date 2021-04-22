4f60c0d1a170c540d9a18de2fef4074cee3eddc8
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-04-22 11:40:42 +0300

    Add check_for_redirect() function
    With try...except and raise

--- script.py
+++ script.py
@@ -9,11 +9,29 @@ path = 'books'
 if not os.path.exists(path):
     os.makedirs(path)
 
+
+def check_for_redirect(response_check):
+    if response_check.history:
+        raise requests.HTTPError
+    else:
+        pass
+
+
 while counter < all_pages:
-    url = f'https://tululu.org/txt.php?id={counter}'
-    response = requests.get(url, verify=False)
-    response.raise_for_status()
-    filename = f'id={counter}.txt'
-    with open(os.path.join(path, filename), 'wb') as file:
-        file.write(response.content)
-    counter += 1
+    payload = {'id': counter}
+    url = f'https://tululu.org/txt.php'
+    response = requests.get(url, verify=False, params=payload)
+    try:
+        check_for_redirect(response)
+        response.raise_for_status()
+        filename = f'id={counter}.txt'
+        with open(os.path.join(path, filename), 'wb') as file:
+            file.write(response.content)
+        counter += 1
+    except requests.HTTPError:
+        counter += 1
+
+
+
+
+
