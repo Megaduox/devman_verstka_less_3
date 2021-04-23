6e57fc4ecefb70c0939b9c8a8d6cdc6c31fb7a0c
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-04-23 17:23:34 +0300

    Download books with titles
    Step 8

--- script.py
+++ script.py
@@ -1,13 +1,8 @@
 import requests
 import os
 
-
-counter = 1
-all_pages = 11
-path = 'books'
-
-if not os.path.exists(path):
-    os.makedirs(path)
+from pathvalidate import sanitize_filename
+from bs4 import BeautifulSoup
 
 
 def check_for_redirect(response_check):
@@ -17,21 +12,28 @@ def check_for_redirect(response_check):
         pass
 
 
-while counter < all_pages:
-    payload = {'id': counter}
-    url = f'https://tululu.org/txt.php'
-    response = requests.get(url, verify=False, params=payload)
-    try:
-        check_for_redirect(response)
-        response.raise_for_status()
-        filename = f'id={counter}.txt'
-        with open(os.path.join(path, filename), 'wb') as file:
-            file.write(response.content)
-        counter += 1
-    except requests.HTTPError:
-        counter += 1
+def download_txt(path='books', counter=1, root_url='https://tululu.org/txt.php', all_pages=11):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    while counter < all_pages:
+        payload = {'id': counter}
+        response = requests.get(root_url, verify=False, params=payload)
+        html_page = f'https://tululu.org/b{counter}/'
+        response_html_page = requests.get(html_page, verify=False)
+        try:
+            check_for_redirect(response)
+            response.raise_for_status()
+            check_for_redirect(response_html_page)
+            response_html_page.raise_for_status()
+            soup = BeautifulSoup(response_html_page.text, 'lxml')
+            book_name_author_all_text_from_h1 = soup.find('h1').text.split('::')
+            book_name = book_name_author_all_text_from_h1[0].strip()
+            filename = sanitize_filename(f'{book_name}.txt')
+            with open(os.path.join(path, filename), 'wb') as file:
+                file.write(response.content)
+            counter += 1
+        except requests.HTTPError:
+            counter += 1
 
 
-
-
-
+download_txt()
