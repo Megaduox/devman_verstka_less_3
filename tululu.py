635310160a884942334c82812ca3ae6d70a01042
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-04-23 16:50:49 +0300

    Add function download_txt()
    Step 7

--- tululu.py
+++ tululu.py
@@ -1,15 +1,33 @@
+import os
 import requests
 
-from bs4 import BeautifulSoup
+from pathvalidate import sanitize_filename
 
-url = 'https://tululu.org/b1/'
-response = requests.get(url, verify=False)
-response.raise_for_status()
 
-soup = BeautifulSoup(response.text, 'lxml')
-book_name_author_all_text_from_h1 = soup.find('h1').text.split('::')
-book_name = book_name_author_all_text_from_h1[0].strip()
-book_author = book_name_author_all_text_from_h1[1].strip()
+def download_txt(url, filename, folder='books/'):
+    """Функция для скачивания текстовых файлов.
+    Args:
+        url (str): Cсылка на текст, который хочется скачать.
+        filename (str): Имя файла, с которым сохранять.
+        folder (str): Папка, куда сохранять.
+    Returns:
+        str: Путь до файла, куда сохранён текст.
+    """
+    # TODO: Здесь ваша реализация
+    response = requests.get(url, verify=False)
+    response.raise_for_status()
+    folder_and_file = os.path.join(folder, f'{sanitize_filename(filename)}.txt')
+    return folder_and_file
 
-print('Автор: ', book_author)
-print('Название книги: ', book_name)
+
+# Примеры использования
+url = 'http://tululu.org/txt.php?id=1'
+
+filepath = download_txt(url, 'Алиби')
+print(filepath)  # Выведется books/Алиби.txt
+
+filepath = download_txt(url, 'Али/би', folder='books/')
+print(filepath)  # Выведется books/Алиби.txt
+
+filepath = download_txt(url, 'Али\\би', folder='txt/')
+print(filepath)  # Выведется txt/Алиби.txt
