554d74ec9b19383c746584d351d1e0dd05c59bb2
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-04-26 13:37:25 +0300

    Download book covers

--- script.py
+++ script.py
@@ -1,6 +1,9 @@
 import requests
 import os
 
+from urllib.parse import urljoin
+from urllib.parse import unquote
+
 from pathvalidate import sanitize_filename
 from bs4 import BeautifulSoup
 
@@ -29,6 +32,8 @@ def download_txt(path='books', counter=1, root_url='https://tululu.org/txt.php',
             book_name_author_all_text_from_h1 = soup.find('h1').text.split('::')
             book_name = book_name_author_all_text_from_h1[0].strip()
             filename = sanitize_filename(f'{book_name}.txt')
+            book_image_short_url = soup.find('div', class_='bookimage').find('img')['src']
+            book_image_full_url = urljoin('https://tululu.org/', book_image_short_url)
             with open(os.path.join(path, filename), 'wb') as file:
                 file.write(response.content)
             counter += 1
@@ -36,4 +41,27 @@ def download_txt(path='books', counter=1, root_url='https://tululu.org/txt.php',
             counter += 1
 
 
-download_txt()
+def download_image(path='images', all_pages=10, counter=5, root_url='https://tululu.org/txt.php'):
+    if not os.path.exists(path):
+        os.makedirs(path)
+    while counter < all_pages:
+        html_page = f'https://tululu.org/b{counter}/'
+        response_html_page = requests.get(html_page, verify=False)
+        try:
+            check_for_redirect(response_html_page)
+            response_html_page.raise_for_status()
+            soup = BeautifulSoup(response_html_page.text, 'lxml')
+            book_image_short_url = soup.find('div', class_='bookimage').find('img')['src']
+            book_image_full_url = urljoin('https://tululu.org/', book_image_short_url)
+            filename = unquote(book_image_full_url.split('/')[-1])
+            response_image = requests.get(book_image_full_url, verify=False)
+            check_for_redirect(response_image)
+            response_image.raise_for_status()
+            with open(os.path.join(path, filename), 'wb') as file:
+                file.write(response_image.content)
+            counter += 1
+        except requests.HTTPError:
+            counter += 1
+
+
+download_image()
