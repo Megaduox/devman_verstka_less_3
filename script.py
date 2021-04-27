3b65c153485c9ac170bdfa781b5e511bc68abd73
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-04-27 15:45:40 +0300

    Parse books genres

--- script.py
+++ script.py
@@ -41,7 +41,7 @@ def download_txt(path='books', counter=1, root_url='https://tululu.org/txt.php',
             counter += 1
 
 
-def download_image(path='images', all_pages=10, counter=5, root_url='https://tululu.org/txt.php'):
+def download_image(path='images', all_pages=10, counter=5):
     if not os.path.exists(path):
         os.makedirs(path)
     while counter < all_pages:
@@ -71,7 +71,7 @@ def parse_book_page(soup):
         print(comment_text)
 
 
-def get_comments(all_pages=11, counter=1, root_url='https://tululu.org/txt.php'):
+def get_comments_and_genres(all_pages=11, counter=1):
     while counter < all_pages:
         html_page = f'https://tululu.org/b{counter}/'
         response_html_page = requests.get(html_page, verify=False)
@@ -81,11 +81,15 @@ def get_comments(all_pages=11, counter=1, root_url='https://tululu.org/txt.php')
             soup = BeautifulSoup(response_html_page.text, 'lxml')
             book_name_author_all_text_from_h1 = soup.find('h1').text.split('::')
             book_name = book_name_author_all_text_from_h1[0].strip()
-            print(book_name)
+            all_book_genres = soup.find('span', class_='d_book')
+            book_genres = all_book_genres.find_all('a')
+            for genr in book_genres:
+                print('Жанр(ы) книги:', genr.text)
+            print('Название книги:', book_name)
             parse_book_page(soup)
             counter += 1
         except requests.HTTPError:
             counter += 1
 
 
-get_comments()
+get_comments_and_genres()
