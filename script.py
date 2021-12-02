39692bf1db7f4eb6b496c9db18ca92c78d430dd4
Megaduox <72254723+Megaduox@users.noreply.github.com>
2021-12-02 17:45:46 +0300

    Improvement part 2: chek-list No# 1,2,4

--- script.py
+++ script.py
@@ -18,9 +18,7 @@ def check_for_redirect(response_check):
 
 def download_txt(response, response_html_page, path='books'):
     os.makedirs(path, exist_ok=True)
-    # check_for_redirect(response)
     response.raise_for_status()
-    # check_for_redirect(response_html_page)
     response_html_page.raise_for_status()
     soup = BeautifulSoup(response_html_page.text, 'lxml')
     book_name_author_all_text_from_h1 = soup.find('h1').text.split('::')
@@ -30,29 +28,21 @@ def download_txt(response, response_html_page, path='books'):
         file.write(response.content)
 
 
-def main(start_book_id, end_book_id, root_url='https://tululu.org/txt.php'):
-    for start_book_id in range(1, end_book_id+1):
-        payload = {'id': start_book_id}
-        response = requests.get(root_url, verify=False, params=payload)
-        html_page = f'https://tululu.org/b{start_book_id}/'
-        response_html_page = requests.get(html_page, verify=False)
-        # download_txt(response, response_html_page)
-        download_image(response_html_page)
-
-
 def download_image(response_html_page, path='images'):
     os.makedirs(path, exist_ok=True)
-    # check_for_redirect(response_html_page)
     response_html_page.raise_for_status()
     soup = BeautifulSoup(response_html_page.text, 'lxml')
-    book_image_short_url = soup.find('div', class_='bookimage').find('img')['src']
-    book_image_full_url = urljoin('https://tululu.org/', book_image_short_url)
-    filename = unquote(book_image_full_url.split('/')[-1])
-    response_image = requests.get(book_image_full_url, verify=False)
-    # check_for_redirect(response_image)
-    response_image.raise_for_status()
-    with open(os.path.join(path, filename), 'wb') as file:
-        file.write(response_image.content)
+    try:
+        book_image_short_url = soup.find('div', class_='bookimage').find('img')['src']
+        book_image_full_url = urljoin('https://tululu.org/', book_image_short_url)
+        filename = unquote(book_image_full_url.split('/')[-1])
+        response_image = requests.get(book_image_full_url, verify=False)
+        # check_for_redirect(response_image)
+        response_image.raise_for_status()
+        with open(os.path.join(path, filename), 'wb') as file:
+            file.write(response_image.content)
+    except AttributeError as e:
+        print(f'Ошибка{e}')
 
 
 def parse_book_comments(soup):
@@ -62,23 +52,22 @@ def parse_book_comments(soup):
         print(comment_text)
 
 
-def get_comments_and_genres(start_page, end_page):
-    for page in range(start_page, end_page):
-        html_page = f'https://tululu.org/b{page}/'
-        response_html_page = requests.get(html_page, verify=False)
-        check_for_redirect(response_html_page)
-        response_html_page.raise_for_status()
-        soup = BeautifulSoup(response_html_page.text, 'lxml')
-        book_name_author_all_text_from_h1 = soup.find('h1').text.split('::')
-        book_name = book_name_author_all_text_from_h1[0].strip()
-        book_author = book_name_author_all_text_from_h1[-1].strip()
-        all_book_genres = soup.find('span', class_='d_book')
-        book_genres = all_book_genres.find_all('a')
-        print('Название книги:', book_name)
-        print('Автор книги:', book_author)
-        for genr in book_genres:
-            print('Жанр(ы) книги:', genr.text)
-        parse_book_comments(soup)
+def get_comments_and_genres(start_page):
+    html_page = f'https://tululu.org/b{start_page}/'
+    response_html_page = requests.get(html_page, verify=False)
+    check_for_redirect(response_html_page)
+    response_html_page.raise_for_status()
+    soup = BeautifulSoup(response_html_page.text, 'lxml')
+    book_name_author_all_text_from_h1 = soup.find('h1').text.split('::')
+    book_name = book_name_author_all_text_from_h1[0].strip()
+    book_author = book_name_author_all_text_from_h1[-1].strip()
+    all_book_genres = soup.find('span', class_='d_book')
+    book_genres = all_book_genres.find_all('a')
+    print('Название книги:', book_name)
+    print('Автор книги:', book_author)
+    for genr in book_genres:
+        print('Жанр(ы) книги:', genr.text)
+    parse_book_comments(soup)
 
 
 def parse_book_page(soup):
@@ -105,7 +94,7 @@ def parse_book_page(soup):
     return book_information
 
 
-if __name__ == '__main__':
+def main(root_url='https://tululu.org/txt.php'):
     parser = argparse.ArgumentParser(description='Скрипт парсинга книг из онлайн-библиотеки tululu.org.'
                                                  'Для работы скрипта задайте два аргумента - с какой по какую'
                                                  'страницу парсить. Если ничего не задать - скрипт'
@@ -114,7 +103,16 @@ if __name__ == '__main__':
     parser.add_argument('start_id', help='Начальная страница', type=int, default=1, nargs='?')
     parser.add_argument('end_id', help='Конечная страница', type=int, default=10, nargs='?')
     args = parser.parse_args()
-    try:
-        main(args.start_id, args.end_id)
-    except requests.HTTPError:
-        print('Ошибка при запросе')
+
+    for start_book_id in range(args.start_id, args.end_id+1):
+        payload = {'id': start_book_id}
+        response = requests.get(root_url, verify=False, params=payload)
+        html_page = f'https://tululu.org/b{start_book_id}/'
+        response_html_page = requests.get(html_page, verify=False)
+        download_txt(response, response_html_page)
+        download_image(response_html_page)
+        get_comments_and_genres(start_book_id)
+
+
+if __name__ == '__main__':
+    main()
